1605f1984cb374c463860064779a96372eb7b89f
Luccas Andre <luccasmelim@yahoo.com.br>
2022-12-14 00:03:39 -0300

    v1.0

---
+++ app.js
@@ -0,0 +1,22 @@
+// Função
+
+function nomeAqui(){
+    console.log("Realizei uma ação")
+}
+
+nomeAqui()
+
+// -------------------------- //
+
+function somaCem(){
+    return 100
+}
+console.log(10 + somaCem())
+
+// -------------------------- //
+
+function soma(number1,number2){
+    return number1 + number2
+}
+
+console.log(soma(1,4))
